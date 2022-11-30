2544e0522dda2b7da4d1e943bf5b0c07fddfad9c
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-11-30 12:20:00 +0100

    1.3.1 + 1.3.2

--- 1.3 Create/script.js
+++ 1.3 Create/script.js
@@ -1,23 +1,57 @@
 /*Modify the script.js to create a new <section> with a random background-color for each learner in your group. This section should contain a paragraph with the name of the learner. Those sections should be appended in the <article>*/
 
-function addElement (nom) {
-    const newSection = document.createElement("section");
-    const newContent = document.createTextNode(nom);
-    newSection.appendChild(newContent);
-    const currentArticle = document.getElementsByTagName("article")[0];
-    currentArticle.appendChild(newSection);
-    console.log(nom);
-}
-
 let array = ["Arno Volts" ,"Aurélien Mariaule" ,"Aurore Lemaire" ,"Benjamin Porsont" ,"Céline Verreydt" ,"Corentin Miserque" ,"Dominique Coppée" ,"Edouard de Romrée de Vichenet" ,"Hugo Goorickx","Jofrey Houyoux" ,"Jonathan Manes" ,"jonathan bajoux" ,"Laura Wilhelmi" ,"Lysie Soyez" ,"Marnie Benalia" ,"Mathilde Cornelis" ,"Milo Bonnet" ,"Nadim El Nakadi" ,"Nathalie Vanden Abeele" ,"Nathalie Goffette" ,"Nour Everaert" ,"Pierre Mauriello" ,"Quentin Bource","Virginie Dourson"];
 
-function pickLearner(array, n) {  
-    let arr = []
+function section(){
+    const newSection = document.createElement("section");
+    const currentArticle = document.getElementsByTagName("article")[0];
+    currentArticle.appendChild(newSection);
+};
+section();
+
+function parag(array, n){
     for (i=1; i<=n; i++){
-        arr.push(array[Math.floor(Math.random() *24)]);
+        function addElement (nom) {
+            const newP = document.createElement("p");
+            const newContent = document.createTextNode(nom);
+            newP.appendChild(newContent);
+            newP.style.backgroundColor = random_p_color();
+            const currentSection = document.getElementsByTagName("section")[0];
+            currentSection.appendChild(newP);
+            console.log(nom);
+        };
+        addElement (array[Math.floor( Math.random() * n )]);
     };
-    return arr
-}
-addElement(pickLearner(array, 1));
+};
+parag(array, 24);
+
+function random_p_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor;
+};
 
 /*If the background is dark the text should be white, if the background is light the text should be black*/
+
+array[changeBgColorNoir, changeBgColorBlanc];
+
+function changeBgColorNoir(){
+    document.body.style.backgroundColor='black'
+    document.body.style.color='white'
+};
+changeBgColorNoir();
+
+function changeBgColorBlanc(){
+    document.body.style.backgroundColor='white'
+    document.body.style.color='black'
+};
+changeBgColorBlanc();
+
+function randomNumber(n){
+    return Math.floor( Math.random() * n );
+};
+randomNumber(1);
+
+array[randomNumber( random.length )](); 
